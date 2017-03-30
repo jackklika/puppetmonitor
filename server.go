dcf164016b5c7ec9b5c4b0882e8e9e0f2afd84e5
jhklika@uwm.edu <jhklika@uwm.edu>
2017-03-30 17:57:12 -0400

    A lot of work before commits!

---
+++ server.go
@@ -0,0 +1,55 @@
+package main
+
+// http://go-database-sql.org/retrieving.html
+
+import (
+	"reflect"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	var data Node
+	// Stores pdbout.json into byte array jsonout
+	jsonout, readerr := ioutil.ReadFile("pdbout.json")
+
+	if readerr != nil {
+		log.Fatal(readerr)
+	}
+
+	parseerr := json.Unmarshal(jsonout, &data)
+	if parseerr != nil {
+		log.Fatal(parseerr)
+	}
+
+	fmt.Println("Here is a list of all nodes:\n")
+	fmt.Println(reflect.TypeOf(data))
+	for _,node := range data {
+		fmt.Println(node.Certname)
+	}
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":1337", nil)
+}
+
+type Node []struct {
+	Deactivated                  interface{} `json:"deactivated"`
+	LatestReportHash             string      `json:"latest_report_hash"`
+	FactsEnvironment             string      `json:"facts_environment"`
+	CachedCatalogStatus          string      `json:"cached_catalog_status"`
+	ReportEnvironment            string      `json:"report_environment"`
+	LatestReportCorrectiveChange interface{} `json:"latest_report_corrective_change"`
+	CatalogEnvironment           string      `json:"catalog_environment"`
+	FactsTimestamp               time.Time   `json:"facts_timestamp"`
+	LatestReportNoop             bool        `json:"latest_report_noop"`
+	Expired                      interface{} `json:"expired"`
+	LatestReportNoopPending      bool        `json:"latest_report_noop_pending"`
+	ReportTimestamp              time.Time   `json:"report_timestamp"`
+	Certname                     string      `json:"certname"`
+	CatalogTimestamp             time.Time   `json:"catalog_timestamp"`
+	LatestReportStatus           string      `json:"latest_report_status"`
+}
